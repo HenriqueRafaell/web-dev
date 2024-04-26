ac37cf1a2e08f4b2e96187e7449ac71c8e715e88
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-26 09:16:02 -0300

    mostrando como que instala o tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {extend: {},
     extend: {},
   },
   plugins: [],
 }
 
+/*para instalar o tailwind no vite, so entrar no site, docs framework guides e achar o vite e sguir o passo a passo*/
